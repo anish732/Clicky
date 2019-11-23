1e2831b93a78204973ea96cfdb20905b880ce4f5
Shital Patel <shital732@yahoo.com>
2019-11-23 14:55:25 -0500

    Updates

---
+++ precache-manifest.4db18f579c809c6fb51b6b6bf3bbfe28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705f0c9ac4ccb238a2fb14301251ec4c",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "43576a90c601a19afe59",
+    "url": "/Clicky/static/css/main.2d915f10.chunk.css"
+  },
+  {
+    "revision": "37215726c2a8ab6fa174",
+    "url": "/Clicky/static/js/2.6d099f0c.chunk.js"
+  },
+  {
+    "revision": "43576a90c601a19afe59",
+    "url": "/Clicky/static/js/main.f61da0dc.chunk.js"
+  },
+  {
+    "revision": "083d930f842b3aa16046",
+    "url": "/Clicky/static/js/runtime-main.49ebdb9a.js"
+  },
+  {
+    "revision": "89413a15a60448dcf86ea27c4f0931d9",
+    "url": "/Clicky/static/media/header.89413a15.jpg"
+  }
+]);
