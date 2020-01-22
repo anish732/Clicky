b6d1fa40e5798ea0fa53a87326883b7f77e5cd6f
Shital Patel <shital732@yahoo.com>
2020-01-22 14:33:05 -0500

    Change target score

--- src/App.js
+++ src/App.js
@@ -5,61 +5,64 @@ import friends from "./friends.json";
 import Title from "./components/Title";
 import Header from "./components/Header";
 
-
 class App extends Component {
-
-  state= {
+  state = {
     count: 0,
     friends,
     clickedImage: [],
     topScore: 0
-
   };
 
   //handleIncrement increase this.state.count by 1
-  handleIncrement = (id) => {
+  handleIncrement = id => {
     console.log(id);
-    if(!this.state.clickedImage.includes(id)){
+    if (!this.state.clickedImage.includes(id)) {
       const oldArray = this.state.clickedImage;
       const newArray = oldArray.concat([id]);
       const shuffled = this.shuffle(this.state.friends);
-      this.setState({ count: this.state.count + 1, clickedImage: newArray, friends: shuffled, topScore: this.state.count+1});
+      this.setState({
+        count: this.state.count + 1,
+        clickedImage: newArray,
+        friends: shuffled,
+        topScore: this.state.count + 1
+      });
+    } else {
+      alert("You lost try again...");
+      this.setState({
+        count: 0,
+        clickedImage: []
+      });
     }
-  else{
-    alert('You lost try again...');
-    this.setState({count:0, clickedImage: []})
-  }
   };
-   shuffle= (a) => {
+  shuffle = a => {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
     }
     return a;
-}
+  };
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <Wrapper>
-        <Title count={this.state.count}
-              topScore={this.state.topScore}
-        />
+        <Title count={this.state.count} topScore={this.state.topScore} />
         <Header></Header>
         {this.state.friends.map(friend => (
           <FriendCard
-           id={friend.id}
-           key={friend.id}
-           image={friend.image}
-           handleIncrement={this.handleIncrement}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+            handleIncrement={this.handleIncrement}
           />
-        ))},
+        ))}
+        ,
       </Wrapper>
     );
-  };
+  }
 }
 
 export default App;
